e8eb21a2a061dc30da66e9f43b79d159e7b8484c
prathamesh111 <prathamsamudra@gmail.com>
2022-03-23 19:00:41 +0530

    get and put to firebase

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,7 @@
 import { Component, EventEmitter, Output } from "@angular/core";
+import { Subject } from "rxjs";
+import { Recipe } from "../recipies/recipe.model";
+import { DataStorageService } from "../shared/data-shared.service";
 
 
 
@@ -10,14 +13,26 @@ import { Component, EventEmitter, Output } from "@angular/core";
 export class HeaderComponent{
     isActionActive:boolean = false;
 
+    newFetched = new Subject<Recipe[]>();
+
 @Output() featureAppSelected = new EventEmitter<string>();
+
+    constructor(private DataStorageService : DataStorageService){}
 
     onselect(feature : string){
         this.featureAppSelected.emit(feature);
     }
 
     onAction(){
-        this.isActionActive=true;
+        this.isActionActive= !this.isActionActive;
+    }
+
+    onSaveData(){
+        this.DataStorageService.storeRecepies();
+    }
+
+    onFetch(){
+        this.DataStorageService.fetchData();
     }
 
 }
---
+++ src/app/shared/data-shared.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { Recipe } from "../recipies/recipe.model";
+import { RecipieService } from "../recipies/recipies.service";
+import {map} from 'rxjs/operators';
+@Injectable({providedIn:"root"})
+export class DataStorageService{
+    constructor(private HttpClient : HttpClient, private recepiService : RecipieService ){
+
+    }
+
+    storeRecepies(){
+      const recepies=  this.recepiService.getRecipies();
+      this.HttpClient.put('https://foodie-app-c2cd1-default-rtdb.firebaseio.com/recepies.json', recepies).subscribe((response)=>{
+        console.log(response);
+      })
+    }
+
+    fetchData(){
+        this.HttpClient.
+        get<Recipe[]>('https://foodie-app-c2cd1-default-rtdb.firebaseio.com/recepies.json')
+        .pipe(map(recipies => {
+          console.log(recipies + "IP");
+          return recipies.map(
+            recipe =>{
+              return {...recipe, ingredients : recipe.ingredients ? recipe.ingredients : []};
+            }
+          );
+        }))
+        .subscribe(response =>{
+            this.recepiService.setRecipies(response);
+        })
+    }
+}
