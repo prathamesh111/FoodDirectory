e657f9eb60cd7e62beaefe6efd8496d1479e8375
prathamesh111 <prathamsamudra@gmail.com>
2022-04-06 10:01:47 +0530

    Revert "auth module and header link validation fix"
    This reverts commit 2c6300df36ee4bd4d696bd21ccb16273ca2cfc04.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import {HttpClientModule} from '@angular/common/http';
+import {HttpClientModule, HTTP_INTERCEPTORS} from '@angular/common/http';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
@@ -10,17 +11,20 @@ import {ButtonModule} from 'primeng/button';
 import {DropdownModule} from 'primeng/dropdown';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { ShoppingListService } from './shopping-list/shoppingList.service';
+import { RecipieService } from './recipies/recipies.service';
+import {AuthComponent} from './auth/auth.component';
+import { AuthInterceptorService } from './auth/auth-interceptor';
 import { RecipiesModule } from './recipies/recipies.module';
 import { ShoppingListModule } from './shopping-list/shoppingList.module';
 import { SharedModule } from './shared/shared.module';
-import { CoreModule } from './core.module';
-import { AuthModule } from './auth/auth.module';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
+    AuthComponent,
   ],
   imports: [
     BrowserModule,
@@ -30,15 +34,14 @@ import { AuthModule } from './auth/auth.module';
     DropdownModule,
     HttpClientModule,
     BrowserAnimationsModule,
+    FormsModule,
     AppRoutingModule,
     ReactiveFormsModule,
     RecipiesModule,
     ShoppingListModule,
-    SharedModule,
-    AuthModule,
-    CoreModule,
+    SharedModule
   ],
-  providers: [],
+  providers: [ShoppingListService,RecipieService, {provide:HTTP_INTERCEPTORS, useClass:AuthInterceptorService, multi:true}],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
