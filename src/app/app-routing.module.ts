a496167343c9de59ffb640c83e7f094030178d6a
prathamesh111 <prathamsamudra@gmail.com>
2022-04-06 09:23:16 +0530

    Revert "resolver to fix no recipe loading on reload"
    This reverts commit 7e689caad4abb3ff44b242d1af1488082a36ea94.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { RecipeEditComponent } from './recipies/recipe-edit/recipe-edit.component';
+import { RecipeStartComponent } from './recipies/recipe-start/recipe-start.component';
+import { RecipieDetailsComponent } from './recipies/recipie-details/recipie-details.component';
+import { RecipiesComponent } from './recipies/recipies.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 
 const appRoutes: Routes = [
   {path:'', redirectTo:'/recipies' ,pathMatch : 'full'},
+  {path:'recipies', component:RecipiesComponent, children:[
+    {path:'', component:RecipeStartComponent},
+    {path:'new', component:RecipeEditComponent},
+    {path:':id', component:RecipieDetailsComponent},
+    {path:':id/edit', component:RecipeEditComponent}
+  ]},
+  
+  
+  {path:'shopping-list', component:ShoppingListComponent}
 ]
 
 @NgModule({
--- src/app/shared/data-shared.service.ts
+++ src/app/shared/data-shared.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from "@angular/core";
 import { HttpClient, HttpParams } from "@angular/common/http";
 import { Recipe } from "../recipies/recipe.model";
 import { RecipieService } from "../recipies/recipies.service";
-import {map, tap} from 'rxjs/operators';
+import {map} from 'rxjs/operators';
 import { AuthService } from "../auth/auth.service";
 @Injectable({providedIn:"root"})
 export class DataStorageService{
@@ -17,26 +17,18 @@ export class DataStorageService{
     }
 
     fetchData(){
-      // just subscribe it once when clicked fetchdata and then done. can also be done by calling unsubscribe immediately
-      // this.authService.user.pipe(take(1)).subscribe(user => { });
-      
-      // we are creating 2 observables at a time so we are piping both of them together by using exhaustmap 
-        return this.HttpClient.
-        get<Recipe[]>('https://foodie-app-c2cd1-default-rtdb.firebaseio.com/recepies.json',
-      )
-      .pipe(
-        map(recipies => {
+        this.HttpClient.
+        get<Recipe[]>('https://foodie-app-c2cd1-default-rtdb.firebaseio.com/recepies.json')
+        .pipe(map(recipies => {
+          console.log(recipies + "IP");
           return recipies.map(
             recipe =>{
               return {...recipe, ingredients : recipe.ingredients ? recipe.ingredients : []};
             }
           );
-        }),
-        tap(recipies => {
-          this.recepiService.setRecipies(recipies);
-        }
-        )
-      );
-     
+        }))
+        .subscribe(response =>{
+            this.recepiService.setRecipies(response);
+        })
     }
 }
