5b4505cf775eb2dda112213847f839bb95264fb8
prathamesh111 <prathamsamudra@gmail.com>
2022-04-06 09:09:25 +0530

    revert to working

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,4 +1,5 @@
 import { NgModule } from '@angular/core';
+<<<<<<< HEAD
 import { PreloadAllModules,  RouterModule, Routes } from '@angular/router';
 
 const appRoutes: Routes = [
@@ -7,6 +8,15 @@ const appRoutes: Routes = [
    loadChildren : ()=>import('./recipies/recipies.module').then(m=>{m.RecipiesModule})},
    {path:'shopping-list', loadChildren: ()=> import ('./shopping-list/shoppingList.module').then(m => m.ShoppingListModule)},
    {path: 'auth', loadChildren : ()=>import('./auth/auth.module').then(m => m.AuthModule)}
+=======
+import { RouterModule, Routes } from '@angular/router';
+import { AuthComponent } from './auth/auth.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+
+const appRoutes: Routes = [
+  {path:'', redirectTo:'/recipies' ,pathMatch : 'full'},
+  {path :'auth', component:AuthComponent}
+>>>>>>> parent of 2c6300d (auth module and header link validation fix)
 ]
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import {HttpClientModule} from '@angular/common/http';
+import {HttpClientModule, HTTP_INTERCEPTORS} from '@angular/common/http';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
@@ -9,15 +10,27 @@ import {InputTextModule} from 'primeng/inputtext';
 import {ButtonModule} from 'primeng/button';
 import {DropdownModule} from 'primeng/dropdown';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+<<<<<<< HEAD
 import { ReactiveFormsModule } from '@angular/forms';
 import { SharedModule } from './shared/shared.module';
 import { CoreModule } from './core.module';
+=======
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { ShoppingListService } from './shopping-list/shoppingList.service';
+import { RecipieService } from './recipies/recipies.service';
+import {AuthComponent} from './auth/auth.component';
+import { AuthInterceptorService } from './auth/auth-interceptor';
+import { RecipiesModule } from './recipies/recipies.module';
+import { ShoppingListModule } from './shopping-list/shoppingList.module';
+import { SharedModule } from './shared/shared.module';
+>>>>>>> parent of 2c6300d (auth module and header link validation fix)
 
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
+    AuthComponent,
   ],
   imports: [
     BrowserModule,
@@ -27,12 +40,19 @@ import { CoreModule } from './core.module';
     DropdownModule,
     HttpClientModule,
     BrowserAnimationsModule,
+    FormsModule,
     AppRoutingModule,
     ReactiveFormsModule,
+<<<<<<< HEAD
     SharedModule,
     CoreModule,
+=======
+    RecipiesModule,
+    ShoppingListModule,
+    SharedModule
+>>>>>>> parent of 2c6300d (auth module and header link validation fix)
   ],
-  providers: [],
+  providers: [ShoppingListService,RecipieService, {provide:HTTP_INTERCEPTORS, useClass:AuthInterceptorService, multi:true}],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
